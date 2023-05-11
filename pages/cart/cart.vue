cb1997844ccb832f1ec316a8ef93e3362852450d
zhaoyunfei <zyf120600@163.com>
2023-05-11 10:52:18 +0800

    购物车：数据初始化

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,22 +1,77 @@
 <template>
 	<view>
-		cart
+		<view class="cart_item">
+			<navigator :url="`/pages/goods_detail/goods_detail?goods_id=${item.goods_id}`" class="goods_item"
+				v-for="item in cartList" :key="item.goods_id">
+
+				<!-- 商品图片 -->
+				<image class="goods_images" :src="item.goods_small_logo || doImage" mode="aspectFit" />
+				<view class="goods_info">
+					<view class="goods_title">{{ item.goods_name }}</view>
+					<view class="goods_price">{{ item.goods_price }}</view>
+				</view>
+			</navigator>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+export default {
+	name: 'cart',
+	data() {
+		return {
+
+		};
+	},
+	methods: {
+
+	},
+	computed: {
+		cartList() {
+			return this.$store.state.cartList
 		}
 	}
+};
 </script>
 
-<style>
+<style lang="scss">
+.cart_item {
+	.goods_item {
+		display: flex;
+		border-bottom: 1rpx solid #ddd;
+		padding: 25rpx;
 
+		image {
+			width: 190rpx;
+			height: 190rpx;
+		}
+
+		.goods_info {
+			flex: 1;
+			font-size: 24rpx;
+			display: flex;
+			justify-content: space-between;
+			flex-direction: column;
+			padding: 20rpx;
+
+			.goods_title {
+				// 多行超出隐藏
+				overflow: hidden;
+				-webkit-line-clamp: 2;
+				text-overflow: ellipsis;
+				display: -webkit-box;
+				-webkit-box-orient: vertical;
+			}
+
+			.goods_price {
+				color: #eb4450;
+
+				&::before {
+					content: '￥';
+					margin-right: 5rpx;
+				}
+			}
+		}
+	}
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -3,10 +3,20 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const store = new Vue.Store({
+const store = new Vuex.Store({
     // 初始化
     state: {
-        
+        cartList: [{
+            goods_id: 888,
+            goods_small_logo: 'http://image2.suning.cn/uimg/b2c/newcatentries/0070177617-000000000618993401_2_400x400.jpg',
+            goods_name:'【包邮送大礼】小米(mi) 小米电视4A 49英寸 标准版 智能网络液晶平板电视机',
+            goods_price: 5899,
+            goods_count: 3,
+            goods_select:true
+        }]
+    },
+    mutations: {
+    
     }
 })
 
